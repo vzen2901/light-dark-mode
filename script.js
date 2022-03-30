219542d945c604873dcd7148fb849c9188de5235
vzen2901 <vzen2901@gmail.com>
2022-03-30 08:44:22 +0700

    change dark light

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
 const nav          = document.getElementById('nav');
 const toggleIcon   = document.getElementById('toggle-icon');
 const image1       = document.getElementById('image1');
@@ -6,43 +7,39 @@ const image2       = document.getElementById('image2');
 const image3       = document.getElementById('image3');
 const textBox      = document.getElementById('text-box');
 
-
-
-
-
-
-
-
 /* Dark Mode Toggle */
 function imgMode(color){
     image1.src = `./img/undraw_proud_coder_${color}.svg`;
     image2.src = `./img/undraw_feeling_proud_${color}.svg`;
     image3.src = `./img/undraw_conceptual_idea_${color}.svg`;
 }
-function darkMode(){
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(225 225 225 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    imgMode('dark');
+function dorkLightMode(islight){
+    nav.style.backgroundColor = islight ? 'rgb(225 225 225 / 50%)' : 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = islight ? 'rgb(0 0 0 / 50%)' : 'rgb(225 225 225 / 50%)';
+    toggleIcon.children[0].textContent = islight ? 'Light Mode' : 'Dark Mode';
+    islight ? toggleIcon.children[1].classList.replace('fa-moon','fa-sun') : toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imgMode(islight ? 'light' : 'dark');
 }
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(225 225 225 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    imgMode('light');
-}
+
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        localStorage.setItem('theme', 'dark'); 
+        dorkLightMode(false);
     }else{
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        localStorage.setItem('theme', 'light');
+        dorkLightMode(true);
     }
 
 }
 toggleSwitch.addEventListener('change', switchTheme);
+//check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        dorkLightMode(false);
+    }
+}
